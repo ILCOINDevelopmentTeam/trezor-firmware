5d633bbd28b615719c3bf0c1b15e2df6809f87a1
grdddj <jiri.musil06@seznam.cz>
2021-09-29 15:10:29 +0200

    new protobuf messages, working for nonarray types

--- core/src/all_modules.py
+++ core/src/all_modules.py
@@ -402,6 +402,8 @@ if not utils.BITCOIN_ONLY:
     import trezor.enums.CardanoTxSigningMode
     trezor.enums.CardanoTxWitnessType
     import trezor.enums.CardanoTxWitnessType
+    trezor.enums.EthereumDataType
+    import trezor.enums.EthereumDataType
     trezor.enums.NEMImportanceTransferMode
     import trezor.enums.NEMImportanceTransferMode
     trezor.enums.NEMModificationType
