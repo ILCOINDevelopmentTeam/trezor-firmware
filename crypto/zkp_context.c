8df848a2e8b4855972b22fc349088a48ef6f07a5
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2021-10-19 20:42:31 +0200

    fixup! feat(crypto): implement randomizable secp256k1_zkp context

--- crypto/zkp_context.c
+++ crypto/zkp_context.c
@@ -1,3 +1,25 @@
+/**
+ * Copyright (c) SatoshiLabs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include <assert.h>
 #include <stdatomic.h>
 #include <stdbool.h>
@@ -13,11 +35,10 @@ static secp256k1_context *context;
 static volatile atomic_flag locked;
 static bool initialized = false;
 
-void secp256k1_context_writeable_randomize(
-    secp256k1_context *context_writeable) {
+void secp256k1_context_writable_randomize(secp256k1_context *context_writable) {
   uint8_t seed[32] = {0};
   random_buffer(seed, sizeof(seed));
-  int returned = secp256k1_context_randomize(context_writeable, seed);
+  int returned = secp256k1_context_randomize(context_writable, seed);
   memzero(seed, sizeof(seed));
   assert(returned == 1);
 }
@@ -39,7 +60,7 @@ void zkp_context_init() {
       secp256k1_context_preallocated_create(context_buffer, context_flags);
   assert(context != NULL);
 
-  secp256k1_context_writeable_randomize(context);
+  secp256k1_context_writable_randomize(context);
 
   atomic_flag_clear(&locked);
   initialized = true;
@@ -60,7 +81,7 @@ const secp256k1_context *zkp_context_get_read_only() {
   return context;
 }
 
-secp256k1_context *zkp_context_acquire_writeable() {
+secp256k1_context *zkp_context_acquire_writable() {
   assert(initialized == true);
 
   // We don't expect the context to be used by multiple threads
@@ -68,7 +89,7 @@ secp256k1_context *zkp_context_acquire_writeable() {
   return context;
 }
 
-void zkp_context_release_writeable() {
+void zkp_context_release_writable() {
   assert(initialized == true);
 
   atomic_flag_clear(&locked);
